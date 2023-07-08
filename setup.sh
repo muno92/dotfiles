711b53c3ef3ac5c058afb85a06559963cad82754
n-ogawa <fy59.kou-729@live.jp>
2023-07-08 21:14:45 +0900

    Firefoxなどで日本語入力できるようにする

--- setup.sh
+++ setup.sh
@@ -11,4 +11,7 @@ ln -s "${DOTFILES_DIR}/.zshrc" "${HOME}/.zshrc"
 
 if [[ $(uname) == 'Darwin' ]]; then
     ln -s "${DOTFILES_DIR}/.zshrc_mac" "${HOME}/.zshrc_mac"
+else
+    mkdir -p "${HOME}/.config/environment.d"
+    ln -s "${DOTFILES_DIR}/.config/environment.d/im.conf" "${HOME}/.config/environment.d/im.conf"
 fi
