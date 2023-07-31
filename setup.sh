73b80838c3093b8e0734898228924b240cf628c7
n-ogawa <fy59.kou-729@live.jp>
2023-07-31 22:01:59 +0900

    php.iniの変更内容を忘れないようにバージョン管理する

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -s "${DOTFILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
 ln -s "${DOTFILES_DIR}/.irbrc" "${HOME}/.irbrc"
 ln -s "${DOTFILES_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
 ln -s "${DOTFILES_DIR}/.zshrc" "${HOME}/.zshrc"
+ln -s "${DOTFILES_DIR}/php/custom.ini" "$(brew --prefix)/etc/php/8.2/conf.d/custom.ini"
 
 if [[ $(uname) == 'Darwin' ]]; then
     ln -s "${DOTFILES_DIR}/.zshrc_mac" "${HOME}/.zshrc_mac"
